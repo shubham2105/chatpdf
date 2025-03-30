ea7633830ce92f5f0174bddd7ddffdac9f1f5b85
Shubham Dhole <shubhamdhole215@gmail.com>
2025-03-30 14:57:43 +0530

    clerk auth and homepage implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,36 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { LogIn } from "lucide-react";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const {userId} = await auth();
+  const isAuth = !!userId 
   return (
-   <div className="flex items-center justify-center min-h-screen">
-       <div className="text-center">
-     <h1 className="text-red-600 mb-4">Hello World</h1>
-     <Button>
-      ShadCN Button
-    </Button>
-     </div>
-   </div>
+    <div className="w-screen min-h-screen bg-gradient-to-r from-slate-200 to-indigo-600">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex  flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton afterSignOutUrl="/"/>
+          </div>
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join millions of students, researchers and professionals to instantly
+            answer questions and understand resarch with AI.
+          </p>
+          <div className="w-full mt-4">
+              {isAuth ? (<h1>fileupload</h1>):(
+                <Link href="/sign-in">
+                    <Button>Login to get started <LogIn className="w- h-4 ml-2"/></Button>
+                </Link>
+              )}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
