9663e1767c4b1c381e29a515c3f71dd3bac751f3
Shubham Dhole <shubhamdhole215@gmail.com>
2025-03-30 20:16:19 +0530

    drizzle-neon db connection configurations

---
+++ src/lib/db/index.ts
@@ -0,0 +1,11 @@
+import { neonConfig, neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+neonConfig.fetchConnectionCache = true;
+
+if(!process.env.DATABASE_URL){
+    throw new Error("Database url not found");
+}
+const sql = neon(process.env.DATABASE_URL)
+
+export const db = drizzle(sql)
